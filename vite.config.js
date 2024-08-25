f7c8453ea3cb93eec164848bcb71619f501c335e
Eden Jin <eden.jin@dbschenker.com>
2024-08-25 12:17:43 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const dighumUrl = process.env.DIGHUM_URL;
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     server: {
-      host: '0.0.0.0'
+      host: '0.0.0.0',
+      proxy: {
+        '/v1/': { // 获取请求中带 /api 的请求
+          target: dighumUrl,  // 后台服务器的源
+          changeOrigin: true
+        }
+      }
     },
     plugins: [
       vue(),
