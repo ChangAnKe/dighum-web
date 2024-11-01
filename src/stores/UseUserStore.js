c143ba59434b1724d720eb32a3b532c302c74785
Eden Jin <eden.jin@dbschenker.com>
2024-11-01 16:57:15 +0800

    add roles function

--- src/stores/UseUserStore.js
+++ src/stores/UseUserStore.js
@@ -4,17 +4,22 @@ import { defineStore } from 'pinia';
 export const useUserStore = defineStore({
     id: 'user', // 存储的唯一标识符
     state: () => ({
-        userInfo: JSON.parse(localStorage.getItem('userInfo')) || null, // 用户信息初始为空
+        userInfo: JSON.parse(localStorage.getItem('userInfo')) || null,
+        roles: JSON.parse(localStorage.getItem('userInfo')).userRoles || [] // 用户信息初始为空
     }),
     actions: {
         // 设置用户信息
         setUserInfo(userInfo) {
             this.userInfo = userInfo;
             localStorage.setItem('userInfo', JSON.stringify(userInfo));
+            this.setRoles(userInfo.userRoles);
         },
         // 清除 userInfo  
         clearUserInfo() {
             this.userInfo = null;
+        },
+        setRoles(roles) {
+            this.roles = roles;
         },
         logout() {
             this.clearUserInfo(); // 清除 userInfo  
@@ -30,5 +35,11 @@ export const useUserStore = defineStore({
         //     const storedUserInfo = JSON.parse(localStorage.getItem('userInfo'));
         //     return storedUserInfo;
         // }
+        hasRole: (state) => (role) => {
+            if (state.roles.includes('ROOT')) {
+                return true;
+            }
+            return state.roles.includes(role);
+        }
     }
 });
